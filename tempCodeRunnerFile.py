6402be6664af23d045bb4e57a794ec8e0eb06f5a
Kartik Attri <attrikartik494@gmail.com>
2026-01-18 20:53:59 +0530

    Day2 started

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+tup = (36, 14, 55, 16)
+print(tup)
+print(id(tup[0]))
+print(id(tup[1]))
+
+#Fetching values 
+#print(tup[0])
+
+#Changing Values
+#tup[0] = 90     (Becox it is immutable so thats why we cant change its value )   
+
+Lis1 = [36, 14, 55, 16]
+print(Lis1)
+print(id(Lis1[0]))
+print(id(Lis1[1]))
